98d9304a48b57466a616f80de7ec22d4e989bc20
mech-studi <mc.studifiziere@gmail.com>
2019-05-13 23:12:54 +0200

    init. spring boot web app with gradle.

---
+++ jobe/src/main/java/ch/uzh/ifi/access/jobe/Quote.java
@@ -0,0 +1,38 @@
+package ch.uzh.ifi.access.jobe;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.jdi.Value;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
